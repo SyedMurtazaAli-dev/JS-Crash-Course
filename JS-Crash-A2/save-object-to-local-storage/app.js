01680b88bef0ad3f893b9bf0f8d2ddbf9f4400f0
Syed Murtaza Ali <syedmurtazaaliofficial@gmail.com>
2023-04-12 03:17:59 +0500

    Q6-A2

---
+++ JS-Crash-A2/save-object-to-local-storage/app.js
@@ -0,0 +1,5 @@
+function saveToLocalStorage(key, value) {
+    localStorage.setItem(key, JSON.stringify(value));
+  }
+  const myObject = JSON.parse(localStorage.getItem("myKey"));
+  console.log(myObject);
