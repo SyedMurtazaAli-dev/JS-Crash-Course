3535455d9de8d71bef370472ef9e7b774c364c03
Syed Murtaza Ali <syedmurtazaaliofficial@gmail.com>
2023-04-12 03:00:37 +0500

    Q3-A2

--- JS-Crash-A2/Adding-Paragraph-Element/app.js
+++ JS-Crash-A2/Adding-Paragraph-Element/app.js
@@ -1,11 +1,9 @@
 function addParagraph(text) {
-
     const para = document.createElement("p");
     para.textContent = text; 
     const body = document.querySelector("body");
     const oldHTML = body.innerHTML;
     const newHTML = oldHTML + para.outerHTML;
     body.innerHTML = newHTML;
-
   }
 addParagraph("hi my name is syed murtaza ali")
